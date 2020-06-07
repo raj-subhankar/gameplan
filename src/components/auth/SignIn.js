71a41c0993fd27cb1f9e6c567b213e4e48f47024
raj-subhankar <subhankar.rj@gmail.com>
2020-06-07 19:25:08 +0530

    Setup login and signup

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import {signIn} from '../../store/actions/authActions'
 
 class SignIn extends Component {
     state = {
@@ -7,13 +9,15 @@ class SignIn extends Component {
     }
     handleChange = (e) => {
         this.setState({
-            [e.target.id]: e.taget.value
+            [e.target.id]: e.target.value
         })
      }
     handleSubmit = (e) => { 
         e.preventDefault()
+        this.props.signIn(this.state)
     }
     render() {
+        const {authError} = this.props
         return (
             <div className="container ">
                 <form onSubmit={this.handleSubmit} className="white">
@@ -31,10 +35,26 @@ class SignIn extends Component {
                     <div className="input-field">
                         <button className="btn pink lighten-1 z-depth-0">Login</button>
                     </div>
+
+                    <div className="red-text center">
+                        {authError ? <p>{authError}</p> : null}
+                    </div>
                 </form>
             </div>
         )
     }
 }
 
-export default SignIn
+const mapStateToProps = (state) => {
+    return{
+        authError: state.auth.authError
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return{
+        signIn: (creds) => dispatch(signIn(creds))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn)
--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import SignedInLinks from './SignedInLinks'
 import SignedOutLinks from './SignedOutLinks'
+import {connect} from 'react-redux'
 
 
 const Navbar = () => {
@@ -16,5 +17,10 @@ const Navbar = () => {
         </nav>
     )
 }
+const mapStateToProps = (state) => {
+    return {
 
-export default Navbar
+    }
+}
+
+export default connect(mapStateToProps)(Navbar)
