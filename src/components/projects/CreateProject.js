6a4fdd1b34d0684eae1eb5064ad5f403b7ba01e6
raj-subhankar <subhankar.rj@gmail.com>
2020-06-07 11:46:35 +0530

    Add Thunk

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -1,9 +1,21 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {createProject} from '../../store/actions/projectActions'
 
 class CreateProject extends Component {
     state = {
         title: "",
         content: ""
+    }
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.createProject(this.state)
     }
     render() {
         return(
@@ -29,4 +41,10 @@ class CreateProject extends Component {
     }
 }
 
-export default CreateProject
+const mapDispatchToProps = (dispatch) => {
+    return{
+        createProject: (project) => dispatch(createProject(project))
+    }
+}
+
+export default connect(null, mapDispatchToProps)(CreateProject)
