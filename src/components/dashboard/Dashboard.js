282e312f7665414d864ea1c82fe5dee7c516af7e
raj-subhankar <subhankar.rj@gmail.com>
2020-06-08 12:13:13 +0530

    Update Notification component

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -16,7 +16,7 @@ class Dashboard extends Component {
                     <div className="col s12 m6">
                         <ProjectList projects={projects}/>
                     </div>
-                    <div className="cols12 m5 offset-m1">
+                    <div className="col s12 m5 offset-m1">
                         <Notifications/>
                     </div>
                 </div>
