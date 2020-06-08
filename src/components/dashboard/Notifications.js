b9e48f0f035535466fb27e9400ac79c0d8830c4f
raj-subhankar <subhankar.rj@gmail.com>
2020-06-08 15:09:12 +0530

    Show notifications

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,21 +1,26 @@
 import React from 'react'
+import moment from 'moment'
 
 const Notifications = (props) => {
-    return(
-        <div className="section">
-            <div className="card z-depth-0">
-                <div className="card-content">
-                    <span className="card-title">Notifications</span>
-                    <ul className="notifications">
-                        <li>Notification</li>
-                        <li>Notification</li>
-                        <li>Notification</li>
-                        <li>Notification</li>
-                    </ul>
-                </div>
-            </div>
+  const { notifications } = props;
+  return (
+    <div className="section">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Notifications</span>
+          <ul className="online-users">
+            { notifications && notifications.map(item =>{
+              return <li key={item.id}>
+                <span className="pink-text">{item.user} </span>
+                <span>{item.content}</span>
+                <div className="note-date grey-text">{moment(item.time.toDate()).fromNow()}</div>
+              </li>
+            })}
+          </ul>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default Notifications
