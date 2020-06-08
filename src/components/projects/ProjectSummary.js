4f1e9ea1f5a9f13774016f53ff2b5bbbf5f3e308
raj-subhankar <subhankar.rj@gmail.com>
2020-06-08 11:41:09 +0530

    Add project update

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -5,7 +5,7 @@ const ProjectSummmary = ({project}) => {
         <div className="card z-depth-0 project-summary">
             <div className="card-content grey-text text-darken-3">
                 <span className="card-title">{project.title}</span>
-                <p>Posted by the Net Ninja</p>
+                <p>Posted by {project.authorFirstName} {project.authorLastName}</p>
                 <p className="grey-text">3rd September, 2 AM</p>
             </div>
         </div>
